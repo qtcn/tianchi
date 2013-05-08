cca70bd5c8479cc26368f59095d2a43fe44e8e8d
XChinux <XChinux@163.com>
2013-05-08 12:51:07 +0800

    fix tcmsexcel.cpp's TcMSExcelPrivate's bug

--- src/file/tcmsexcel.cpp
+++ src/file/tcmsexcel.cpp
@@ -20,11 +20,13 @@ public:
 
     TcMSExcel*  q_ptr;
 
+#if defined(Q_OS_WIN)
     QAxObject*  excel;
     QAxObject*  books;
     QAxObject*  book;
     QAxObject*  sheets;
     QAxObject*  sheet;
+#endif
     QString     filename;
     QString     sheetName;
 };
@@ -38,15 +40,13 @@ TcMSExcelPrivate::TcMSExcelPrivate(TcMSExcel *qptr)
     , sheets(NULL)
     , sheet(NULL)
 #endif // Q_OS_WIN
-    , filename("")
-    , sheetName("")
 {
 }
 
 TcMSExcelPrivate::~TcMSExcelPrivate()
 {
 #if defined(Q_OS_WIN)
-    if ( !  excel->isNull() )
+    if (!excel->isNull())
     {
         excel->dynamicCall("Quit()");
     }
@@ -63,11 +63,11 @@ void TcMSExcelPrivate::construct()
 #if defined(Q_OS_WIN)
     destory();
     excel->setControl("Excel.Application");
-    if ( excel->isNull() )
+    if (excel->isNull())
     {
         excel->setControl("ET.Application");
     }
-    if ( ! excel->isNull() )
+    if (!excel->isNull())
     {
         books = excel->querySubObject("Workbooks");
     }
@@ -79,13 +79,13 @@ void TcMSExcelPrivate::destory()
 #if defined(Q_OS_WIN)
     TC_FREE(sheet );
     TC_FREE(sheets);
-    if ( book != NULL && ! book->isNull() )
+    if (book != NULL && ! book->isNull())
     {
         book->dynamicCall("Close(Boolean)", false);
     }
     TC_FREE(book );
     TC_FREE(books);
-    if ( excel != NULL && ! excel->isNull() )
+    if (excel != NULL && ! excel->isNull())
     {
         excel->dynamicCall("Quit()");
     }
@@ -108,11 +108,11 @@ TcMSExcel::~TcMSExcel()
 
 bool TcMSExcel::create(const QString& filename)
 {
-	bool ret = false;
+    bool ret = false;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
     d->construct();
-    if ( d->books != NULL && ! d->books->isNull() )
+    if (d->books != NULL && ! d->books->isNull())
     {
         d->books->dynamicCall("Add");
         d->book   = d->excel->querySubObject("ActiveWorkBook");
@@ -129,7 +129,6 @@ bool TcMSExcel::open(const QString& filename)
 {
     bool ret = false;
 #if defined(Q_OS_WIN)
-
     Q_D(TcMSExcel);
     d->construct();
     if ( d->books != NULL && ! d->books->isNull() )
@@ -172,7 +171,7 @@ void TcMSExcel::kick()
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
 
-    if ( d->excel != NULL && ! d->excel->isNull() )
+    if (d->excel != NULL && !d->excel->isNull())
     {
         d->excel->setProperty("Visible", true);
     }
@@ -190,10 +189,10 @@ QStringList TcMSExcel::sheetNames()
     QStringList ret;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheets != NULL && ! d->sheets->isNull() )
+    if (d->sheets != NULL && !d->sheets->isNull())
     {
         int sheetCount = d->sheets->property("Count").toInt();
-        for( int i=1; i<=sheetCount; i++ )
+        for (int i = 1; i <= sheetCount; i++)
         {
             QAxObject* sheet = d->sheets->querySubObject("Item(int)", i);
             ret.append(sheet->property("Name").toString());
@@ -213,7 +212,7 @@ void TcMSExcel::setVisible(bool value)
 {
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->excel != NULL && ! d->excel->isNull() )
+    if (d->excel != NULL && !d->excel->isNull())
     {
         d->excel->setProperty("Visible", value);
     }
@@ -224,7 +223,7 @@ void TcMSExcel::setCaption(const QString& value)
 {
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->excel != NULL && ! d->excel->isNull() )
+    if (d->excel != NULL && !d->excel->isNull())
     {
         d->excel->setProperty("Caption", value);
     }
@@ -236,14 +235,14 @@ bool TcMSExcel::addBook()
     bool ret = false;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->excel != NULL && ! d->excel->isNull() )
+    if (d->excel != NULL && !d->excel->isNull())
     {
         TC_FREE(d->sheet );
         TC_FREE(d->sheets);
         TC_FREE(d->book  );
         TC_FREE(d->books );
         d->books = d->excel->querySubObject("WorkBooks");
-        ret = d->books != NULL && ! d->books->isNull();
+        ret = d->books != NULL && !d->books->isNull();
     }
 #endif // Q_OS_WIN
     return ret;
@@ -255,11 +254,11 @@ bool TcMSExcel::currentSheet()
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
     TC_FREE(d->sheet);
-    if ( d->excel != NULL && ! d->excel->isNull() )
+    if (d->excel != NULL && !d->excel->isNull())
     {
         TC_FREE(d->sheet);
         d->sheet = d->excel->querySubObject("ActiveWorkBook");
-        ret = d->sheet != NULL && ! d->sheet->isNull();
+        ret = d->sheet != NULL && !d->sheet->isNull();
         d->sheetName = ret ? d->sheet->property("Name").toString() : "";
     }
 #endif // Q_OS_WIN
@@ -271,7 +270,7 @@ bool TcMSExcel::setCurrentSheet(int index)
     bool ret = false;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheets != NULL && ! d->sheets->isNull() )
+    if (d->sheets != NULL && !d->sheets->isNull())
     {
         TC_FREE(d->sheet);
         d->sheet = d->sheets->querySubObject("Item(int)", index);
@@ -287,7 +286,7 @@ int TcMSExcel::sheetCount()
     int ret = 0;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheets != NULL && ! d->sheets->isNull() )
+    if (d->sheets != NULL && ! d->sheets->isNull())
     {
         ret = d->sheets->property("Count").toInt();
     }
@@ -299,7 +298,7 @@ void TcMSExcel::cellFormat(int row, int col, const QString& format)
 {
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheet != NULL && ! d->sheet->isNull() )
+    if (d->sheet != NULL && ! d->sheet->isNull())
     {
         QAxObject* range = d->sheet->querySubObject("Cells(int, int)", row, col);
         range->setProperty("NumberFormatLocal", format);
@@ -312,7 +311,7 @@ void TcMSExcel::cellAlign(int row, int col, Alignment hAlign, Alignment vAlign)
 {
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheet != NULL && ! d->sheet->isNull() )
+    if (d->sheet != NULL && !d->sheet->isNull())
     {
         QAxObject* range = d->sheet->querySubObject("Cells(int, int)", row, col);
         range->setProperty("HorizontalAlignment", hAlign);
@@ -327,7 +326,7 @@ QVariant TcMSExcel::read(int row, int col)
     QVariant ret;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheet != NULL && ! d->sheet->isNull() )
+    if (d->sheet != NULL && ! d->sheet->isNull())
     {
         QAxObject* range = d->sheet->querySubObject("Cells(int, int)", row, col);
         //ret = range->property("Value");
@@ -342,7 +341,7 @@ void TcMSExcel::write(int row, int col, const QVariant& value)
 {
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheet != NULL && ! d->sheet->isNull() )
+    if (d->sheet != NULL && ! d->sheet->isNull())
     {
         QAxObject* range = d->sheet->querySubObject("Cells(int, int)", row, col);
         range->setProperty("Value", value);
@@ -356,7 +355,7 @@ bool TcMSExcel::usedRange(int& rowStart, int& colStart, int& rowEnd, int& colEnd
     bool ret = false;
 #if defined(Q_OS_WIN)
     Q_D(TcMSExcel);
-    if ( d->sheet != NULL && ! d->sheet->isNull() )
+    if (d->sheet != NULL && ! d->sheet->isNull())
     {
         QAxObject* urange  = d->sheet->querySubObject("UsedRange");
         rowStart = urange->property("Row"   ).toInt();
